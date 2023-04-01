349e2f24b5feae355bb8b832bd98e26518fb9c1d
Nikojuu <come_on12345@hotmail.com>
2023-04-01 22:48:43 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9f8e12a1-019a-493a-88ef-ebca65e96348';
+export const MODAL_CLOSE_SEC = 2.5;
